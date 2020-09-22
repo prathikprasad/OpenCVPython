26f040f0d28e6897e92ce570ba960c07f3092642
Prathik P <36961962+prathikprasad@users.noreply.github.com>
2020-09-22 17:27:31 +0530

    new chapter

--- Chapter7.py
+++ Chapter7.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-
 def stackImages(scale,imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
@@ -32,18 +31,38 @@ def stackImages(scale,imgArray):
         ver = hor
     return ver
 
+def empty(a):
+    pass
 
-img = cv2.imread('Resources/prat.jpg')
-imgGrey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+path = 'Resources/lambo.png'
+cv2.namedWindow("TrackBar")
+cv2.resizeWindow("TrackBar",640,240)
+cv2.createTrackbar("Hue Min", "TrackBar", 0,179, empty)
+cv2.createTrackbar("Hue Max", "TrackBar", 19,179, empty)
+cv2.createTrackbar("Sat Min", "TrackBar", 110,255, empty)
+cv2.createTrackbar("Sat Max", "TrackBar", 240,255, empty)
+cv2.createTrackbar("Val Min", "TrackBar", 153,255, empty)
+cv2.createTrackbar("Val Max", "TrackBar", 255,255, empty)
 
-imgStack = stackImages(0.5,([img,imgGrey,img],[img,img,img]))
+while True:
+    img = cv2.imread(path)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBar")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBar")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBar")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBar")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBar")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBar")
+    print(h_min, h_max, s_min, s_max, v_min, v_max )
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    imgRes = cv2.bitwise_and(img,img, mask=mask)
+    # cv2.imshow("Original", img)
+    # cv2.imshow("HSV", imgHSV)
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("ImgRes", imgRes)
 
-# imgHor = np.hstack((img,img))
-# imgVer = np.vstack((img,img))
-#
-#
-# cv2.imshow("Hori", imgHor)
-# cv2.imshow("Vert", imgVer)
-cv2.imshow("Stack", imgStack)
-
-cv2.waitKey(0)
+    imgStack = stackImages(0.4,([img, imgHSV],[mask, imgRes]))
+    cv2.imshow("result", imgStack)
+    cv2.waitKey(1)
--- Chapter8.py
+++ Chapter8.py
@@ -31,19 +31,45 @@ def stackImages(scale,imgArray):
         hor= np.hstack(imgArray)
         ver = hor
     return ver
+def getContours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print(area)
+        if area>500:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt, True)
+            #print(peri)
+            approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+            print(len(approx))
+            objCor = len(approx)
+            x, y, w, h = cv2.boundingRect(approx)
+
+            if objCor == 3: objectType = "Tri"
+            elif objCor == 4:
+                aspRatio = w/float(h)
+                if aspRatio>0.95 and aspRatio<1.05: objectType = "Square"
+                else: objectType = "Rectangle"
+            elif objCor>4: objectType = "Circle"
+            else: objectType = "None"
+            cv2.rectangle(imgContour, (x,y),(x+w, y+h),(0,255,0),2)
+            cv2.putText(imgContour, objectType,
+                        (x+(w//2)-10,y+(h//2)),
+                        cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 0.8, (0,0,0), 1)
 
 
-img = cv2.imread('Resources/prat.jpg')
-imgGrey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+path = 'Resources/shapes.png'
+img = cv2.imread(path)
+imgContour = img.copy()
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7,7), 1)
+imgCanny = cv2.Canny(imgBlur, 50, 50)
+getContours(imgCanny)
 
-imgStack = stackImages(0.5,([img,imgGrey,img],[img,img,img]))
-
-# imgHor = np.hstack((img,img))
-# imgVer = np.vstack((img,img))
-#
-#
-# cv2.imshow("Hori", imgHor)
-# cv2.imshow("Vert", imgVer)
+imgStack = stackImages(0.7,([img, imgGray, imgBlur],
+                            [imgCanny, imgContour, imgBlur]))
 cv2.imshow("Stack", imgStack)
-
+# cv2.imshow("Original",img)
+# cv2.imshow("Gray",imgGray)
+# cv2.imshow("Blur",imgBlur)
 cv2.waitKey(0)
